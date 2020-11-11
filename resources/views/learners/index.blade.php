8f1c166bd5cbdcbb9406ef966e9009632ef4b4a7
Angel Jabal <christinejabal5@gmail.com>
2020-11-11 13:36:39 +0800

    first commit

---
+++ resources/views/learners/index.blade.php
@@ -0,0 +1,32 @@
+@extends('base')
+
+@section('content')
+
+<div class="container index">
+<h1>Learners</h1>
+<table class="table table-bordered table-striped table-sm">
+    <thead>
+        <tr>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Level</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach($learners as $learner)
+
+        <tr>
+            <td>{{$learner->lname}}</td>
+            <td>{{$learner->fname}}</td>
+            <td>{{$learner->level}}</td>
+            <td>{{$learner->status}}</td>
+        </tr>
+
+        @endforeach
+    </tbody>
+</table>
+</div>
+
+@stop
